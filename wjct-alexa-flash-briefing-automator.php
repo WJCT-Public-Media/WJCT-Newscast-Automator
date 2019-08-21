2119f0e489e3a8195ae91af58d188af4325593e7
Ray <rhollister@wjct.org>
2019-08-21 11:46:12 -0400

    version 0.05 (testing do not use)

--- wjct-alexa-flash-briefing-automator.php
+++ wjct-alexa-flash-briefing-automator.php
@@ -1,9 +1,9 @@
 <?php
    /*
    Plugin Name: WJCT Alexa Flash Briefing Automator
-   Plugin URI: https://www.wjct.org
+   Plugin URI: https://github.com/RayHollister/WJCT-Alexa-Flash-Briefing-Automator
    description: A plugin that automatically updates the WJCT Alexa Flash Briefing when the NPR One newscast has been uploaded.
-   Version: 0.04
+   Version: 0.05
    Author: Ray Hollister
    Author URI: https://rayhollister.com
    License:
@@ -15,8 +15,112 @@
    // The URL of the MP3 file uploaded to NPR One.
    $mp3url = 'https://media.publicbroadcasting.net/wjct/newscast/newscast.mp3';
 
-   // Add a widget to the dashboard.
+ ////////////////////////////////////////////
+ /*         CRON JOB STARTS HERE           */
+ /////////////////////////////////////////////
+ // Credit Jay Versluis
+ // https://bit.ly/2HgDfUW
 
+ // unschedule event upon plugin deactivation
+ function cronstarter_deactivate()
+ {
+     // find out when the last event was scheduled
+     $timestamp = wp_next_scheduled('WJCT_flash_briefing_automator_cron');
+     // unschedule previous event if any
+     wp_unschedule_event($timestamp, 'WJCT_flash_briefing_automator_cron');
+ }
+ register_deactivation_hook(__FILE__, 'cronstarter_deactivate');
+ // create a scheduled event (if it does not exist already)
+ function cronstarter_activation()
+ {
+     if (!wp_next_scheduled('WJCT_flash_briefing_automator_cron')) {
+         // wp_schedule_event(time(), 'every5minutes', 'WJCT_flash_briefing_automator_cron');
+         wp_schedule_event(time(), 'everyminute', 'WJCT_flash_briefing_automator_cron');
+
+     }
+ }
+ // and make sure it's called whenever WordPress loads
+ add_action('wp', 'cronstarter_activation');
+
+ // here's the function we'd like to call with our cron job
+ function newscast_checker()
+ {
+     global $mp3url;
+     // get the datetime when the newscast was last updated
+     $lastupdate = get_option('lastupdated');
+
+     $headers = get_headers($mp3url);
+
+     // Get the HTTP last modified header
+     $recentlastmod = substr($headers[7], 15, 29);
+     // convert string to Unix Timestamp
+     $recenttimestamp = strtotime($recentlastmod);
+     // format timestamp and convert to local timezone
+     $recentupdate = date("m/d/Y h:i:s A T", $recenttimestamp);
+
+     if ($recentupdate != $lastupdate) {
+         programmatically_create_post();
+     }
+     else {
+       // Just testing if the cron is running!
+       programmatically_create_post();
+     }
+
+     // store the most recent datetime the newscast was updated in the website database
+     update_option('lastupdated', $recentupdate);
+     // update_option('lastupdated', 'tents');
+ }
+
+ // hook that function onto our scheduled event:
+ add_action('WJCT_flash_briefing_automator_cron', 'newscast_checker');
+ // CUSTOM INTERVALS
+ // by default we only have hourly, twicedaily and daily as intervals
+ // to add your own, use something like this - the example adds 'weekly'
+ // http://codex.wordpress.org/Function_Reference/wp_get_schedules
+
+ // add another interval
+ function cron_add_minute($schedules)
+ {
+     // Adds once every minute to the existing schedules.
+     $schedules['everyminute'] = array(
+        'interval' => 60,
+        'display' => __('Once Every Minute')
+     );
+     return $schedules;
+ }
+ add_filter('cron_schedules', 'cron_add_minute');
+ // add another interval
+ function cron_add_fiveminutes($schedules)
+ {
+     // Adds once every 5 minutes to the existing schedules.
+     $schedules['every5minutes'] = array(
+        'interval' => 300,
+        'display' => __('Once Every 5 Minutes')
+     );
+     return $schedules;
+ }
+ add_filter('cron_schedules', 'cron_add_fiveminutes');
+
+ // add another interval
+ function cron_add_fifteenminutes($schedules)
+ {
+     // Adds once every 15 minutes to the existing schedules.
+     $schedules['every15minutes'] = array(
+        'interval' => 900,
+        'display' => __('Once Every 15 Minute')
+     );
+     return $schedules;
+ }
+ add_filter('cron_schedules', 'cron_add_fifteenminutes');
+ /*
+ more info here:
+ https://bit.ly/31XBQuz
+ https://github.com/versluis
+ THE REST OF THE CODE IS NOT USED FOR THE CRON FUNCTION
+ */
+
+
+   // Add a widget to the dashboard.
    function WJCT_Latest_Newscast_add_dashboard_widget()
    {
        wp_add_dashboard_widget(
@@ -102,7 +206,7 @@ function programmatically_create_post()
 
     // $title = 'My Example Post';
     // Set the title to the most recent updated date time
-    $title = 'Flash Briefing ' . date("m/d/Y H:i:s", $recenttimestamp);
+    $title = 'Flash Briefing ' . date("m/d/Y H:i:s A", $recenttimestamp);
 
     // 1605 is WJCT's 'News Flash' category id
     $category_ids = array(1605);
